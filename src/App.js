852e2a245b5934b1327584e89e7513565611cdfb
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-27 18:17:20 -0500

    comments added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-'use strict';
 
 //REACT
 //JSX - Javascript XML   -> se parece a html pero ~
 
 //define una variable jsx que es un parrafo, pero no se exporta solo
-var template = React.createElement(
-  'p',
-  null,
-  'Esto es codigo JSX y no html, esto va a cambiar en el background '
-);
+var template = <p>Esto es codigo JSX y no html, esto va a cambiar en el background </p>
 
 //mandamos a buscar el div con el id = app
-var appRoot = document.getElementById('app');
+var appRoot = document.getElementById('app')
 
 //objecto de ReactDOM que va a llevar nuestros parrafo al index.html
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template, appRoot)
