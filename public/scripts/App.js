6d2e0ea842df81eccb546f6209250a4bc03496e8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-07-09 18:23:31 -0500

    Two components created

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,17 +1,119 @@
-'use strict';
+"use strict";
 
 //REACT
 //JSX - Javascript XML   -> se parece a html pero ~
 
+/*
+Mantener a babel en escucha:
+babel src/App.js --out-file=public/scripts/App.js --presets=env,react --watch
+
+live-server public
+
+*/
+
 //define una variable jsx que es un parrafo, pero no se exporta solo
 var template = React.createElement(
-  'p',
-  null,
-  'Esto es codigo JSX y no html, esto va a cambiar en el background '
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Esto es codigo JSX y no html, esto va a cambiar en el background "
+	),
+	React.createElement(
+		"p",
+		null,
+		"oye!"
+	),
+	React.createElement("input", { type: "submit", value: "Clickame" }),
+	React.createElement(
+		"ul",
+		null,
+		React.createElement(
+			"li",
+			null,
+			"Pruebalo ya"
+		),
+		React.createElement(
+			"li",
+			null,
+			"Infinidad de posibilidades"
+		),
+		React.createElement(
+			"li",
+			null,
+			"Muchos colores!"
+		)
+	)
+);
+
+//string
+var activo = "Estudiante activo del tec";
+
+//json
+var state = {
+	userName: "Eduardo",
+	universidad: "ITISTMO",
+	genero: "Masculino",
+	ciudad: "Oaxaca, Mexico"
+
+	//variable
+};var fecha = Date();
+
+function getUniversity() {
+	//Condicional, si se cumple se asigna lo que va despues de ?, si no, se asigna lo que va
+	//despues de :
+	return state.universidad != "" ? state.universidad : "Universidad Desconocida";
+}
+
+function cityIs() {
+	//inline estilo
+	return React.createElement(
+		"h3",
+		{ style: { color: "blue" } },
+		state.ciudad
+	);
+}
+
+function getCity() {
+	//operador ternario
+	return state.ciudad != "" ? cityIs() : "Ciudad Desconocida";
+}
+
+var templateTwo = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Este template no se puede cargar al mismo tiempo que el template1"
+	),
+	React.createElement(
+		"h3",
+		null,
+		state.userName
+	),
+	React.createElement(
+		"h3",
+		null,
+		getUniversity()
+	),
+	React.createElement(
+		"h3",
+		null,
+		state.genero
+	),
+	getCity(),
+	React.createElement(
+		"p",
+		null,
+		"Hola! La fecha es: ",
+		fecha
+	)
 );
 
 //mandamos a buscar el div con el id = app
 var appRoot = document.getElementById('app');
 
 //objecto de ReactDOM que va a llevar nuestros parrafo al index.html
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/App.js
+++ src/App.js
@@ -2,11 +2,75 @@
 //REACT
 //JSX - Javascript XML   -> se parece a html pero ~
 
+/*
+Mantener a babel en escucha:
+babel src/App.js --out-file=public/scripts/App.js --presets=env,react --watch
+
+live-server public
+
+*/
+
 //define una variable jsx que es un parrafo, pero no se exporta solo
-var template = <p>Esto es codigo JSX y no html, esto va a cambiar en el background </p>
+var template = (
+				<div>
+					<h1>Esto es codigo JSX y no html, esto va a cambiar en el background </h1>
+					<p>oye!</p>
+					<input type="submit" value="Clickame"/>
+					<ul>
+						<li>Pruebalo ya</li>
+						<li>Infinidad de posibilidades</li>
+						<li>Muchos colores!</li>
+					</ul>
+				</div>
+			);
+
+//string
+var activo = "Estudiante activo del tec";
+
+//json
+var state = {
+	userName: "Eduardo",
+	universidad: "ITISTMO",
+	genero: "Masculino",
+	ciudad: "Oaxaca, Mexico"
+}
+
+//variable
+var fecha = Date()
+
+function getUniversity() {
+	//Condicional, si se cumple se asigna lo que va despues de ?, si no, se asigna lo que va
+	//despues de :
+	return state.universidad != "" ? state.universidad : "Universidad Desconocida"
+}
+
+function cityIs(){
+	//inline estilo
+	return <h3 style={{color:"blue"}}>{state.ciudad}</h3>
+}
+
+function getCity(){
+	//operador ternario
+	return state.ciudad != "" ? cityIs() : "Ciudad Desconocida"
+}
+
+var templateTwo = 
+	(
+		<div>
+		{/*acakocnapskca*/}
+		<h1>Este template no se puede cargar al mismo 
+			tiempo que el template1</h1>
+		<h3>{state.userName}</h3>
+		<h3>{getUniversity()}</h3>
+		<h3>{state.genero}</h3>
+		{getCity()}
+		<p>Hola! La fecha es: {fecha}</p>
+
+		</div>
+	);
 
 //mandamos a buscar el div con el id = app
 var appRoot = document.getElementById('app')
 
 //objecto de ReactDOM que va a llevar nuestros parrafo al index.html
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
